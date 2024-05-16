6715a37cc5020cebdcd0b2e32556f5099ccc870a
Karsten <17077140+codeKarsten@users.noreply.github.com>
2024-05-16 14:11:52 +0200

    Split up Design + Code

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const textInput = document.getElementById('textInput');
+    const imageInput = document.getElementById('imageInput');
+    const inputImage = document.getElementById('inputImage');
+    const letterInput = document.getElementById('letterInput');
+    const canvas = document.getElementById('outputCanvas');
+    const context = canvas.getContext('2d');
+
+    // Set the default image on page load
+    inputImage.src = "letters.png";
+
+    imageInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            inputImage.src = e.target.result;
+            inputImage.style.display = 'block';
+        };
+
+        reader.readAsDataURL(file);
+    });
+
+    document.getElementById('generateButton').addEventListener('click', generateImage);
+
+    function generateImage() {
+        const text = textInput.value;
+        const letterInputValue = letterInput.value;
+        const letterWidth = inputImage.width / letterInputValue.length;
+        const letterHeight = inputImage.height;
+        const lines = text.split('\n');
+        let maxWidth = 0;
+
+        lines.forEach(function (line) {
+            maxWidth = Math.max(maxWidth, line.length);
+        });
+
+        canvas.width = maxWidth * letterWidth;
+        canvas.height = lines.length * letterHeight;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        lines.forEach(function (line, index) {
+            let pos = 0;
+            for (let i = 0; i < line.length; i++) {
+                const char = line.charAt(i);
+                const charIndex = getCharIndex(char, letterInputValue);
+                if (char === ' ' || charIndex !== -1) {
+                    context.drawImage(inputImage, charIndex * letterWidth, 0, letterWidth, letterHeight, pos * letterWidth, index * letterHeight, letterWidth, letterHeight);
+                }
+                pos++;
+            }
+        });
+
+        canvas.style.display = 'block';
+    }
+
+    function getCharIndex(char, letterInput) {
+        return letterInput.indexOf(char);
+    }
+
+    window.generateImage = generateImage; // Make the function accessible globally
+});
